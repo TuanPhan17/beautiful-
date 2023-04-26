0fbd972f2340a9250484ccd07dcd3f2301cc3dcd
Patrick Bagtas <122561605+onlovelamp@users.noreply.github.com>
2023-04-26 11:26:10 -0700

    Add files via upload

---
+++ client/src/views/BeautifulCreate.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import axios from 'axios';
+
+export default function BeautifulPeople(props) {
+
+
+
+
+
+
+    return (
+        <div>
+            <h1>Be Beautiful Create</h1>
+        </div>
+    )
+}
---
+++ client/src/views/BeautifulPost.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import axios from 'axios';
+
+export default function BeautifulPeople(props) {
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            <h1>Be Beautiful Home </h1>
+        </div>
+    )
+
+
+}
