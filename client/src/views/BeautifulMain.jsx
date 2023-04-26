0a70867bf4ffb2e59e401dda36ce3c640980fb15
Patrick Bagtas <122561605+onlovelamp@users.noreply.github.com>
2023-04-26 14:46:29 -0700

    Update BeautifulMain.jsx

--- client/src/views/BeautifulMain.jsx
+++ client/src/views/BeautifulMain.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import bullseye from '../img/bullseye.png'
+
+import {container,Row,column,text,img,  } from 'react-bootstrap'
 
 import axios from 'axios';
 
 export default function BeautifulPeople(props) {
+    const [blogs, setBlogs] = useState([]);
 
-
-
-
-
-
+useEffect(() => {
+    axios.get('http://localhost:8000/api/blog')
+    .then(response => {
+        console.log(response.data);
+        setBlogs(response.data);
+    })
+    .catch((error) => {console.log(error);
+    })
+}, []);
 
 
     return (
-        <div>
-            <h1>Be Beautiful Home</h1>
-</div>
-    )
+        <container>
+            <div>
+                <h1 className='text-center'>Beautiful People</h1>
 
-
+                <img src={bullseye} className='App-logo mx-auto d-block w-25 h-25' alt="logo" />
+                    <div key = {blogs.blog}/>
+                <div className='row text-center'>
+                    <p className='col fw-bold'>Sports</p>
+                    <p className='col fw-bold'>Inspiration</p>
+                    <p className='col fw-bold'>Beauty</p>
+                </div>
+            </div>
+        </container>
+    );
 }
