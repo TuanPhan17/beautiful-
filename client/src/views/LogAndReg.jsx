4e4e158ffb6b71086f0de4f141451f5050d412f9
Patrick Bagtas <122561605+onlovelamp@users.noreply.github.com>
2023-04-28 10:43:36 -0700

    Update LogAndReg.jsx

--- client/src/views/LogAndReg.jsx
+++ client/src/views/LogAndReg.jsx
@@ -4,35 +4,115 @@ import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-export default function BeautifulPeople(props) {
 
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
+export default function ShowAllProductView(props) {
+    const [authors, setAuthors] = useState([]);
     const navigate = useNavigate();
+
     const [validationErrors, setValidationErrors] = useState(null);
 
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-
-    const handleCreateSubmitButton = (event) => {
+    const handleNewAuthorSubmit = (event) => {
         event.preventDefault();
 
-        const newBlog = {
-            name: name,
-            description: description
+        const newUser = {
+            email: email,
+            password: password,
+            username: username,
+            confirmPassword: confirmPassword,
         };
 
-        axios.post('http://localhost:8000/api/blog/new', newBlog)
-            .then(response => {
-                console.log(response.data);
-                navigate("/")
-            })
 
+        // creating New USER---------------
+        axios
+            .post('http://localhost:8000/api/users', newUser)
+            .then((res) => {
+                navigate('/');
+                console.log(res.data);
+            })
             .catch((error) => {
                 setValidationErrors(error?.response?.data?.errors);
                 console.log(error);
-            })
+            });
+
+
     }
 
+    return (
+        <div className='d-flex justify-between m-5' >
+
+            <form
+                onSubmit={(event) => {
+                    handleNewAuthorSubmit(event);
+                }}
+            >
+                <label>Email:</label>
+                <input className="mb-3 " type="text"
+                    onChange={(event) => {
+                        setEmail(event.target.value)
+                    }}
+                />
+                <br />
+
+                <label>Username:</label>
+                <input type="text" className="mb-3 "
+                    onChange={(event) => {
+                        setUsername(event.target.value)
+                    }}
+                />
+                <br />
+
+                <label>Password:</label>
+                <input type="text" className="mb-3 "
+                    onChange={(event) => {
+                        setPassword(event.target.value)
+                    }}
+                />
+                <br />
+
+                <label>Confirm Password:</label>
+                <input type="text" className="mb-3 "
+                    onChange={(event) => {
+                        setConfirmPassword(event.target.value)
+                    }}
+                />
+                <br />
+
+                <button className='bg-purple-300 rounded-full w-20 '>Register</button>
+            </form>
+
+
+            <form
+                onSubmit={(event) => {
+                    handleNewAuthorSubmit(event);
+                }}
+            >
+                <label>Email:</label>
+                <input type="text" className="mb-3 "
+                    onChange={(event) => {
+                        setEmail(event.target.value)
+                    }}
+                />
+                <br />
+                <label className='mb-3'>Password:</label>
+                <input type="text" 
+                    onChange={(event) => {
+                        setPassword(event.target.value)
+                    }}
+                />
+                <br />
+
+                <button className='bg-purple-300 rounded-full w-20 '>Login</button>
+            </form>
+
+        </div>
+    )
+
+}
 
 
 
